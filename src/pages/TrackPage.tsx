c17a9230d8b416c073b70c0b897f30ac106de34d
rishabh794 <rishabhbuchha123456789@gmail.com>
2025-11-25 18:51:56 +0530

    add /track route for track progress

---
+++ src/pages/TrackPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '@/components/event/EventBanner'
+import EventPage from '../components/event/EventPage'
+
+const Track = () => {
+  return (
+    <div>
+      <Hero/>
+      <EventPage/>
+    </div>
+  )
+}
+
+export default Track
